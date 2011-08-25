7bce8ee3cf45886c33fbfcbc9de38af178705f87
Dario Ghilardi <darioghilardi@gmail.com>
2011-08-26 00:55:10 +0200

    Added FOS_userbundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,8 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Quark\QuestionBundle\QuarkQuestionBundle(),
             new Quark\AnswerBundle\QuarkAnswerBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Quark\UserBundle\QuarkUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Quark/UserBundle/QuarkUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Quark\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QuarkUserBundle extends Bundle
+{
+}
